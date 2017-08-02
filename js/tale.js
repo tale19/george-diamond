e764dd6f35b576a0bc4d75cdb17cadc9c07ea46e
Stefan <stefan.tatic@yahoo.com>
2017-08-02 17:49:47 +0200

    read more/read less completed, whitespace between spans removed. add if condition

--- js/tale.js
+++ js/tale.js
@@ -1,72 +1,79 @@
 function main() {
-	/* Dynamically show "read more" on top news if text is too long */
+	// Dynamically show "read more" on top news if text is too long 
 	function toggleNews() {
 	;
 		console.log("clicked");
 	}
 
+	// Locate the news span and its parent
 	var span = document.getElementById('thisSpan');
 	var parent = document.getElementById('parent-paragraph');
-	// console.log(parent);
-	// console.log(span);
-	// console.log(span.innerText);
-	// console.log(span.innerText.length);
-	// console.log(span.childNodes[0]);
-	var previewText = span.innerText.substring(0,100);
-	var extraText = span.innerText.substring(100, span.innerText.length);
+	console.log(parent);
+	console.log(span);
+	console.log(span.innerText);
+	console.log(span.innerText.length);
+	console.log(span.childNodes[0]);
+
+	// create substrings from main span (thisSpan)
+	var previewText = span.innerText.substring(0,150);
+	var extraText = span.innerText.substring(150, span.innerText.length);
 	console.log(previewText);
 	console.log(extraText);
-	// console.log(span);
-	// console.log(extraText.length);
+	console.log(span);
+	console.log(extraText.length);
+
+	// create a new span that will hold the extra text
 	var hiddenContent = document.createElement('span');
 	// console.log(hiddenContent);
 	var extraTextNode = document.createTextNode(extraText);
 	hiddenContent.appendChild(extraTextNode);
+	parent.appendChild(hiddenContent);
 
+	// overwrite the whole content of thisSpan with just preview text
 	var previewTextNode = document.createTextNode(previewText);
 	console.log(previewTextNode);
 	span.replaceChild(previewTextNode, span.childNodes[0]);
 	console.log(span);
 	console.log(hiddenContent);
 	$(hiddenContent).addClass('hidden-content');
-	$(span).addClass('newClass');
+	// $(span).addClass('newClass');
 
+	// now create Read More and Read Less
 	var anchorExpander = document.createElement('a');
 	var expanderText = document.createTextNode('Read more...');
 	anchorExpander.appendChild(expanderText);
 	console.log(anchorExpander);
 	parent.appendChild(anchorExpander);
-	$(parent).attr('id', 'news-expander');
+	$(anchorExpander).attr('id', 'news-expander');
+
+	var anchorCollapser = document.createElement('a');
+	var collapserText = document.createTextNode('Read less...');
+	anchorCollapser.appendChild(collapserText);
+	console.log(anchorCollapser);
+	parent.appendChild(anchorCollapser);
+	$(anchorCollapser).attr('id', 'news-collapser');
+
+	// remove automatically generated whitespace 
+	// between thisSpan and hidden-content span:
+	parent.childNodes[4].previousSibling.data = "";
+	console.log(parent.childNodes[4].previousSibling);
 
 
-
-	// console.log(hiddenContent);
-	// var hiddenText = document.createTextNode(extraText);
-	// hiddenContent.appendChild(extraText);
-	// console.log(extraText);
-	// console.log(span.innerText);
-
-	// if (span.innerText.length > 100) {
-	// 	// console.log(hiddenText);
-	// 	// hiddenText.css('color', 'blue');
-	// }
-
-
-
+	// now add functionality to anchor expander/collapser
 	$('#news-expander').click(function() {
 		console.log('klik');
-		console.log($(this.nextElementSibling));
+		console.log($(this.previousSibling));
 		// $(this.nextSibling.nextSibling).toggleClass('hidden-content');
-		$(this.nextElementSibling).toggleClass('hidden-content');
+		$(this.previousSibling).toggleClass('hidden-content');
 		$(this).hide();
 		$('#news-collapser').show();
 	});
 
 	$('#news-collapser').click(function() {
 		console.log('klikić');
-		console.log($(this.previousElementSibling));
+		console.log($(this.previousSibling.previousSibling));
 		// $(this.nextSibling.nextSibling).toggleClass('hidden-content');
-		$(this.previousElementSibling).toggleClass('hidden-content');
+		$(this.previousSibling.previousSibling).toggleClass('hidden-content');
 		$(this).hide();
 		$('#news-expander').show();
 	});
