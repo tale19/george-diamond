1e07058e481c3414906f0fa8187e449d1598bcf4
Stefan <stefan.tatic@yahoo.com>
2017-08-02 15:53:56 +0200

    added span with class hidden-content, but it doesn't work yet

--- js/tale.js
+++ js/tale.js
@@ -6,9 +6,33 @@ function main() {
 	}
 
 	var span = document.getElementById('thisSpan');
+	// var parent = document.getElementsByClassName('top-news-preview');
 	// var convert = $('#thisSpan'.firstChild);
 	console.log(span);
 	console.log(span.innerText.length);
+	console.log(span.innerText);
+	var extraText = span.innerText.substring(100, span.innerText.length);
+	span = span.innerText.substring(0,100);
+	console.log(span);
+	console.log(extraText.length);
+	var hiddenContent = document.createElement('span');
+	console.log(hiddenContent);
+	var node = document.createTextNode(extraText);
+	hiddenContent.appendChild(node);
+	// console.log(hiddenContent);
+	$(span).addClass('')
+	$(hiddenContent).addClass('hidden-content');
+	// console.log(hiddenContent);
+	// var hiddenText = document.createTextNode(extraText);
+	// hiddenContent.appendChild(extraText);
+	// console.log(extraText);
+	// console.log(span.innerText);
+
+	// if (span.innerText.length > 100) {
+	// 	// console.log(hiddenText);
+	// 	// hiddenText.css('color', 'blue');
+	// }
+
 
 
 	$('#news-expander').click(function() {
