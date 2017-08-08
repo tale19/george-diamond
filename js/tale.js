b7fd21f50249da2eec3fb2d11f8edb3556be09cf
Stefan <stefan.tatic@yahoo.com>
2017-08-09 01:31:36 +0200

    page header text fadeIn animation added

--- js/tale.js
+++ js/tale.js
@@ -1,96 +1,95 @@
+	// Animate header text
+	
+	var text = document.getElementById('about-george').innerText;
+	for (i=0; i < text.length; i++) {
+	var span = document.createElement('span');
+	var node = document.createTextNode(text[i])
+	span.appendChild(node);
+	// span.style.opacity = "0.4";
+	// span.style.color = "blue";
+	console.log(span);
+	}
+
+
+
 function main() {
+	// Animate header text
+	var content = document.getElementById('about-george').innerHTML;
+	var text = document.getElementById('about-george').innerText;
+	var contentLength = content.length;
+
+	for (i=0; i < text.length; i++) {
+	  var span = document.createElement('span');
+	  var node = document.createTextNode(text[i]);
+	  span.appendChild(node);
+	  document.getElementById('about-george').appendChild(span);
+	  document.getElementById('about-george').innerHTML[i] = span;
+	}
+	var onlySpans = document.getElementById('about-george').innerHTML.substring(contentLength);
+	document.getElementById('about-george').innerHTML = onlySpans;
+	console.log(document.getElementById('about-george'));
+	$('#about-george span').delay(300).hide().fadeIn(1400);
+
 	// Dynamically show "read more" on top news if text is too long 
-	function toggleNews() {
-	;
-		console.log("clicked");
-	}
+	var showChar = 150;
+	var ellipsestext = "...";
+	var moretext = "Read more";
+	var lesstext = "Read less";
+	$('.more').each(function() {
+		// var content = $(this).html();
+		var content = this.innerHTML;
+		var text = this.innerText;
+		console.log(text);
+ 		// var text = this.innerText;
+ 		
+  		var node = document.createElement("P");
+  		var textnode = document.createTextNode("text here");
+  		node.appendChild(textnode);
+		document.getElementById("wrapper").appendChild(node);
+  		node.innerHTML = 'test';
+		console.log(text.length);
+		console.log(content); 
+		console.log(content.includes('</a>' && 'Diamond'));
+		if(text.length > showChar) {
 
-	// Locate the news span and its parent
-	var span = document.getElementsByClassName('top-news-body');
-	var parent = document.getElementsByClassName('top-news-paragraph');
+			var c = content.substr(0, showChar);
+			var h = content.substr(showChar, content.length - showChar);
 
-	for (s=0; s<3; s++) {
-		// console.log(span[s]);
-		// console.log(parent[s]);
-		// console.log(span[s]);
-		// console.log(span[s].innerText);
-		// console.log(span[s].innerText.length);
-		// console.log(span[s].childNodes[0]);
+				// this checks if var c is in the middle of an element
+				if(c.includes('<') && c.indexOf('</') == -1) {
+					console.log('var c is split in the middle of an element');
+					var pera = text[showChar];
+					var diff = (content.indexOf('</') + 10 + content.indexOf(pera));
+					console.log(diff);
+					console.log(text[showChar]);
+					c = content.substr(0, diff);
+					h = content.substr(diff, content.length - diff);
+					// c = text.substr(0, showChar);
+					// h = text.substr(showChar, text.length - showChar);
+				}
+			console.log(c.length);
 
-		// console.log(parent[s]);
-		// console.log(span[s]);
-		// console.log(span[s].innerText);
-		// console.log(span[s].innerText.length);
-		// console.log(span[s].childNodes[0]);
+			var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
-		// create substrings from main span (thisSpan)
-		var previewText = span[s].innerText.substring(0,150);
-		var extraText = span[s].innerText.substring(150, span[s].innerText.length);
-		console.log(previewText);
-		console.log(extraText);
-		console.log(span[s]);
-		console.log(extraText.length);
+			$(this).html(html);
+		}
 
-		// create a new span that will hold the extra text
-		var hiddenContent = document.createElement('span');
-		// console.log(hiddenContent);
-		var extraTextNode = document.createTextNode(extraText);
-		hiddenContent.appendChild(extraTextNode);
-		parent[s].appendChild(hiddenContent);
+	});
 
-		// overwrite the whole content of thisSpan with just preview text
-		var previewTextNode = document.createTextNode(previewText);
-		console.log(previewTextNode);
-		span[s].replaceChild(previewTextNode, span[s].childNodes[0]);
-		console.log(span[s]);
-		console.log(hiddenContent);
-		$(hiddenContent).addClass('hidden-content');
-		// $(span).addClass('newClass');
-
-		// now create Read More and Read Less
-		var anchorExpander = document.createElement('a');
-		var expanderText = document.createTextNode('Read more...');
-		anchorExpander.appendChild(expanderText);
-		console.log(anchorExpander);
-		parent[s].appendChild(anchorExpander);
-		$(anchorExpander).addClass('news-expander');
-
-		var anchorCollapser = document.createElement('a');
-		var collapserText = document.createTextNode('Read less...');
-		anchorCollapser.appendChild(collapserText);
-		console.log(anchorCollapser);
-		parent[s].appendChild(anchorCollapser);
-		$(anchorCollapser).addClass('news-collapser');
-
-		/* remove automatically generated whitespace 
-		between thisSpan and hidden-content span: */
-		parent[s].childNodes[4].previousSibling.data = "";
-		console.log(parent[s].childNodes[4].previousSibling);
+	$(".morelink").click(function(){
+		if($(this).hasClass("less")) {
+			$(this).removeClass("less");
+			$(this).html(moretext);
+		} else {
+			$(this).addClass("less");
+			$(this).html(lesstext);
+		}
+		$(this).parent().prev().toggle();
+		$(this).prev().toggle();
+		return false;
+	});
 
 
-		// now add functionality to anchor expander/collapser
-		$('.news-expander').click(function() {
-			console.log('klik');
-			console.log($(this.previousSibling));
-			$(this.previousSibling).removeClass('hidden-content');
-			// $(this).find('.hidden-content').toggleClass('hidden-content');
-			// $('.hidden-content').toggleClass('hidden-content');
-			$(this.nextSibling).show();
-			$(this).hide();
-		});
-
-		$('.news-collapser').click(function() {
-			console.log('klikić');
-			console.log($(this.previousSibling.previousSibling));
-			// $(this.nextSibling.nextSibling).toggleClass('hidden-content');
-			$(this.previousSibling.previousSibling).addClass('hidden-content');
-			// $('.hidden-content').toggleClass('hidden-content');
-			$(this.previousSibling).show();
-			$(this).hide();
-		});
-
-		console.log(parent[2]);
-	}
 }
 
 $(document).ready(main);
