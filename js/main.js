f4f9ca8e602b34682e1cd032ddbba70ac46c58cb
Stefan <stefan.tatic@yahoo.com>
2017-08-29 21:17:56 +0200

    js files separated, other pages added

--- js/main.js
+++ js/main.js
@@ -94,7 +94,8 @@ function main() {
 		// 	$(this).addClass('animationExecutedOnce');
 		// });
 
-		$h2.addClass('gradient');
+		// $h2.addClass('gradient');
+		$h2.addClass('italic');
 
 	}
 
@@ -120,7 +121,7 @@ function main() {
 
 	function animateEntireHeading() {
 		animateh1();
-		// animateh2();
+		animateh2();
 		executeHeaderTextAnimation();
 		$heading.children().addClass('animationExecutedOnce');
 	}
@@ -139,74 +140,6 @@ function main() {
 	console.log($heading.children());
 	console.log($h1);
 	console.log($h2);
-
-
-
-	// *** Dynamically show "read more" on top news if text is too long ***
-	var charCount = 350;
-	var $newsBody = $('.news-body');
-	// create preview
-	if ($newsBody.text().length > charCount) {
-		$newsBody.each(function() {
-
-			// create expander
-			var $expander = $('<a/>', {"class": "expandNews"}); // "" around class make it more compatible with less flexible browsers
-			$expander.text('...Read More');
-			// create collapser
-			var $collapser = $('<a/>', {"class": "collapseNews"}); // "" around class make it more compatible with less flexible browsers
-			$collapser.text('...Read Less');
-
-			// create full view after expansion
-			var $originalHTML = $(this).html();
-			var $fullview = [$originalHTML, $collapser]; // string (HTML) + Object [a.collapse]
-			$(this).data("original", $fullview);
-
-
-			// create preview
-			var $datum = $(this).find('.news-date:first');
-			var $previewText = $(this).text().substring($(this).find('.news-date:first').text().length + 11, charCount);
-			var $preview = [$datum, $previewText, $expander]; // Object [span] + string + Object [a.expand]
-			$(this).html($preview);
-			$(this).data("truncatedHTML", $(this).html());
-		});
-	};
-
-	// expand on click
-	$(document).on("click", ('.expandNews'), function() {
-		var $retrieveOriginal = $(this).parent().data("original");
-		$(this).parents('.news-body').html($retrieveOriginal);
-	});
-
-	// collapse on click
-	$(document).on("click", ('.collapseNews'), function(){
-		$truncatedNews = $(this).parents('.news-body').data("truncatedHTML");
-		$(this).parents('.news-body').html($truncatedNews);
-	});
-
-	// Dynamically define container height and set overflow when a news article is expanded
-	console.log($('#top-news'));
-	console.log(document.getElementById('top-news'));
-	document.getElementById('top-news').style.height = "" + document.getElementById('top-news').scrollHeight + "px";
-	document.getElementById('top-news').style.overflow = "auto";
-	document.getElementById('top-news-image').style.height = "" + document.getElementById('top-news').scrollHeight + "px";
-
-
-	// *** Jumbotron listed items appearing one after another on scroll ***
-	// var amazingShowsHeight = document.getElementById('amazing-shows-list').scrollHeight;
-	// document.getElementById('amazing-shows-list').style.height = "" + amazingShowsHeight + "px";
-	// var $amazingShows = $('#amazing-shows-list li');
-
-	// $(window).scroll(function(){
-	// 	// This is where we use the function to detect if "$amazingShows" is scrolled into view, and when it is add the class ".animationExecutedOnce" to the <p> child element
-	// 	if(elementScrolled($amazingShows) && !($amazingShows.hasClass('animationExecutedOnce'))) {	// if $amazingShows is scrolled into view (if elemsScrolled == true)
-	// 		// Your function here
-	// 		$amazingShows.each(function(i) {
-	// 			$(this).delay(150 * i).animate({left: '15px'}, 400);
-	// 			$(this).delay(150 * i).animate({left: '0'}, 400);
-	// 			$(this).addClass('animationExecutedOnce');
-	// 		});
-	// 	}
-	// });
 
 
     // Replace all SVG images with inline SVG
