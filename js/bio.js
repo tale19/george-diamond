1463b2f194e77cc5cbd999f87fbab14bb73e20ce
Stefan <stefan.tatic@yahoo.com>
2017-10-12 23:10:11 +0200

    restyling, first commit

---
+++ js/bio.js
@@ -0,0 +1,10 @@
+
+
+var $slika = $(".slika img");
+$slika.each(function() {
+	var $visina = $(this).parents('.row').find('.tekst').height();
+	$(this).css('max-height', $visina);
+	console.log($visina);
+}); 
+
+$('body').particleground();
