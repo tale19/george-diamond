64dd4159f899329b7387dfb0c74e173cde9ec140
Stefan <stefan.tatic@yahoo.com>
2017-10-20 22:04:24 +0200

    news page draft

--- English/news.view.php
+++ English/news.view.php
@@ -8,6 +8,7 @@
 <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="../css/tale.css">
+<link rel="stylesheet" type="text/css" href="../css/news.css">
 </head>
 
 <body>
@@ -19,24 +20,36 @@ require 'navbar.view.php';
 <div id="content">
 <h3>News</h3>
 
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-sm-4 news-image"></div>
-    <div class="col-sm-8">
-      <div class="news-article">
-        <h4 class="news-headline">George Diamond in the Superfinals of Serbia's Got Talent TV Show!</h4>
-        <div class="news-body">
-        <p>
-          <span class="news-date">8th June 2017 - </span>Months of hard work and preparation have worked in George's favor, as he entered the Superfinals of the Serbia's Got Talent contest! Being chosen as one of the superfinalists of the show by the TV audience's SMS votes has proven that not only he is a top notch illusionist, but also an entertainer with a great energy and charisma! The illusion he chose to present at the Superfinals, which was broadcasted live in Serbia, as well as in several neighboring countries, is his version of the famous Metamorphosis, invented and performed by legendary Houdini - the most famous magician ever! George's gorgeous assistant is handcuffed, placed in a bag, and locked in a box on stage. George steps on the lid of the box, covers himself for a brief second, and instantly a miracle happens - the girl is now on top of the box instead of George, who is later found locked and handcuffed in the box! The judges commented that his act was absolutely stunning, and that his performance was on the highest level! Click on image to watch the video!
-        </p>
-        <p>
-          Morbi pretium hendrerit tincidunt. Donec aliquet, tortor vel feugiat feugiat, orci dolor luctus ante, at laoreet tortor libero eu nunc. Praesent ut tellus volutpat, facilisis lacus in, ultrices dolor. Nam interdum est a arcu luctus, et ultrices augue venenatis. Fusce nec nisl id nisi lacinia congue non aliquet orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum id elit sit amet leo ullamcorper varius.
-        </p>
-       </div> <!-- news body -->
-    </div> <!-- news article -->
-  </div>
+<div class="container">
 
-</div> <!-- row -->
+  <div class="news-content row">
+    <article class="col-sm-6 col-md-8 col-sm-push-6 col-md-push-4 news-article">
+      <h4 class="news-headline">The article on TV Pink's internet portal</h4>
+      <div class="news-body">
+        <p><span class="news-date">2nd June 2017</span></p>
+        <p>Amazingly well done! George Diamond surpasses all his previous performances leaving judges in amazement! - says the title of the article published on TV Pink's internet portal. The article describes the shock and disbelief among the judges  upon seeing George's act calling him one of the most talented illusionists in the whole region who has managed to shock millions of TV viewers again and stating that his performances are talked about weeks after each episode he took part in!  Click HERE to read the article, or click on the photo on the left to see it as PDF.</p>
+        <p>Morbi pretium hendrerit tincidunt. Donec aliquet, tortor vel feugiat feugiat, orci dolor luctus ante, at laoreet tortor libero eu nunc. Praesent ut tellus volutpat, facilisis lacus in, ultrices dolor. Nam interdum est a arcu luctus, et ultrices augue venenatis. Fusce nec nisl id nisi lacinia congue non aliquet orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum id elit sit amet leo ullamcorper varius.KRAJRKAJKRAJ</p>
+      </div> <!-- news body -->
+      <div class="news-preview-expander"><span>Read full article</span></div>
+    </article> <!-- news-article -->
+    <div class="col-sm-6 col-md-4 col-sm-pull-6 col-md-pull-8 news-misc">
+      <div class="news-preview-image-container">
+        <img src="../images/news-dummy-1.webp">
+        <span class="news-preview-image-caption">Caption caption caption<span>
+      </div>
+      <div class="news-share">
+        <span>Share this news</span> <span>FB</span>
+        <span>Tw</span>
+        <span>viber</span>
+        <span>wtsapp</span>
+      </div>
+    </div>
+  </div> <!-- news content, row -->
+
+
+
+<div style="bottom:0; margin-top:50px; text-align: center; background-color: red;">PAGINATOR</div>
+
 </div> <!-- container-fluid -->
 
 
