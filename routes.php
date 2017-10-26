74a1d77790cf74dfc107a3634804a44627b741b6
Stefan <stefan.tatic@yahoo.com>
2017-10-26 21:33:58 +0200

    .htaccess, routes added

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php 
+
+$router->define([
+	'index' => 'controllers/index.php',
+	'' => 'controllers/index.php',
+	'about' => 'controllers/about.php',
+	'about/culture' => 'controllers/about-culture.php',
+	'contact' => 'controllers/contact.php'
+]);
