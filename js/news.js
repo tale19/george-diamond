2bfe8048f947c34cec9e61dc5c28a28ce83b19c8
Stefan <stefan.tatic@yahoo.com>
2017-11-22 22:18:51 +0100

    methods work on page load, fix to appear on scroll

--- js/news.js
+++ js/news.js
@@ -10,14 +10,17 @@ function main() {
 	
 // first, create a getter that returns the window parameters
 	function getWindowOffset() {
-		var $windowTop = $(window).scrollTop() + 67 // 67px is the navbar height;
+		var $windowTop = $(window).scrollTop() + 67; // 67px is the navbar height;
 		var $windowHeight = $(window).height();
 		var $windowBottom = $windowTop + $windowHeight;
 		return [$windowTop, $windowHeight, $windowBottom];
 	}
-		console.log('window scrolltop: ' + getWindowOffset()[0]);
-		console.log('window viewport height: ' + getWindowOffset()[1]);
-		console.log('window bottom view: ' + getWindowOffset()[2]);
+
+	console.log('window scrolltop: ' + getWindowOffset()[0]);
+	console.log('window viewport height: ' + getWindowOffset()[1]);
+	console.log('window bottom view: ' + getWindowOffset()[2]);
+
+
 // next, get offsets of all the news image boxes
 // create a global getter for elements' position
 	function getElementVerticalPosition($elem) {
@@ -28,12 +31,22 @@ function main() {
 		return [$elementTopPosition, $elementBottomPosition];
 	}
 
+// create a method that checks if an element is in the viewport using the window offset and element's parameters
+	function inViewport($elem) {
+		if (getElementVerticalPosition($elem)[0] > getWindowOffset()[0] && getElementVerticalPosition($elem)[1] < getWindowOffset()[2]) {
+			// console.log('element is in Viewport');
+			return true;
+		} 
+		// else {
+		// 	console.log('not in Viewport');
+		// }
+	}
+
+
+	
 // then, pass each news image container to the method to get its position
 	var $newsImageBox = $('.news-image-container');
 	// var positions = [];
-	// console.log(getElementVerticalPosition($(this))[0]);
-	// var $newsBoxTop;
-	// var $newsBoxBottom;
 	$newsImageBox.each(function(){
 		// positions.push(getElementVerticalPosition($(this)));
 		// var $newsBoxTop = getElementVerticalPosition($(this))[0];
@@ -43,7 +56,7 @@ function main() {
 		console.log(getElementVerticalPosition($(this))[1]);
 		// getElementVerticalPosition($(this));
 		// return [$newsBoxTop, $newsBoxBottom];
-		if (getElementVerticalPosition($(this))[0] > getWindowOffset()[0] && getElementVerticalPosition($(this))[1] < getWindowOffset()[1]) {
+		if (inViewport($(this))) {
 			$(this).find('img').addClass('appear');
 			console.log('opa');
 		} 
@@ -51,17 +64,17 @@ function main() {
 		// 	$(this).find('img').removeClass('appear');
 		// }
 	});
-
-
-		// console.log(positions);
 	$(window).scroll(function() {
 		// $clientHeight = $(window).clientHeight;
-
 		console.log('window scrolltop: ' + getWindowOffset()[0]);
-		console.log('window viewport height: ' + getWindowOffset()[1]);
-		console.log('window bottom view: ' + getWindowOffset()[2]);
+		// console.log('window viewport height: ' + getWindowOffset()[1]);
+		// console.log('window bottom view: ' + getWindowOffset()[2]);
+		console.log(inViewport($newsImageBox));
 
 	});
+
+		// console.log(positions);
+
 
 
 
