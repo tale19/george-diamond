b98b904ac3a6129d642e80a3b9fbf3d2ca3531e7
Stefan <stefan.tatic@yahoo.com>
2017-11-28 19:41:18 +0100

    shows page - jumbotron links animation

--- js/shows.js
+++ js/shows.js
@@ -1,4 +1,34 @@
 function main() {
+	// *** SMOOTHLY SCROLL TO SHOW FROM JUMBOTRON LINKS ***
+
+
+	// first, create an array of targets:
+	// var targets = {};
+	// targets['#shows-illusion-shortcut'] = '#shows-illusion';
+	// targets['#shows-stage-shortcut'] = '#shows-stage';
+	// targets['#shows-private-shortcut'] = '#shows-private';
+	// targets['#shows-quickchange-shortcut'] = '#shows-quickchange';
+	// targets['#shows-chinese-shortcut'] = '#shows-chinese';
+	// targets['#shows-short-shortcut'] = '#shows-short';
+	// targets['#shows-parlor-shortcut'] = '#shows-parlor';
+	// targets['#shows-kids-shortcut'] = '#shows-kids';
+
+	$showShortcut = $('.shows-full-list-item');
+	$showShortcut.on('click', function(e){
+		e.preventDefault;
+		var shortcut = '#' + $(this).attr('id');
+		var target = $(this).data('target');
+		console.log(shortcut + ' has target ' + target);
+		$('html, body').animate({
+        	scrollTop: $('#' + target).offset().top - 67
+    }, 1200);
+	});
+
+
+
+
+
+
 	// *** SCROLLTOP ***
 	
 	// first, make scrolltop button visible only when there's an offset 
