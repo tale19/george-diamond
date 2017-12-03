990d9a8b04b5f8ba20a557e52eeccb59a9abcdaf
Stefan <stefan.tatic@yahoo.com>
2017-12-03 14:38:50 +0100

    fb comments box added to article

--- views/en/article.view.php
+++ views/en/article.view.php
@@ -11,6 +11,16 @@
 <link rel="stylesheet" type="text/css" href="<?=$config['url']?>css/tale.css">
 </head>
 <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
 <?php 
   require 'views/en/partials/navbar.view.php';
   // require 'page-header.view.php';
@@ -20,9 +30,9 @@
 
 <div class="container">
 
-  <h3><?= $article->getTitle(); ?></h3>
   <div class="news-article-box row">
     <article class="col-sm-6 col-md-8 col-sm-push-6 col-md-push-4 news-article">
+      <h3><?= $article->getTitle(); ?></h3>
       <div class="news-content">
         <span class="news-date"><?= $article->getDate()->format('D, jS M Y'); ?> - </span>
         <?= $article->getContent(); ?>
@@ -43,15 +53,16 @@
   </div>  <!-- .news-article-box.row -->
 
   <div id="back-to-news">
-    <a href="/news" class="btn btn-primary">Back to all news</a>
+    <a href="/news" class="btn btn-primary"><i class="fa fa-chevron-left" aria-hidden="true"></i> Back to all news</a>
     <a href="/" class="btn btn-round btn-success"><strong>Home Page</strong></a>
   </div>
-</div> <!-- .container -->
 
 <!-- <div style="bottom:0; margin-top:50px; text-align: center; background-color: red;">Comment box</div> -->
 
-</div> <!-- #article-content -->
+  <div class="fb-comments" data-href="<?=$config['url']; ?>news/article/<?= $articleId; ?>" data-numposts="5"></div>
 
+</div> <!-- .container -->
+</div> <!-- #article-content -->
 
   
 
