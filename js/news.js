cecf6f2d0252b82f41286330894a68786c024d0d
Stefan <stefan.tatic@yahoo.com>
2017-12-12 18:59:40 +0100

    footer pushed to the bottom of the page if the page content doesn't fill the viewport

--- js/news.js
+++ js/news.js
@@ -46,7 +46,15 @@ function main() {
     	e.preventDefault();
 	});
 
+// *** PUSH FOOTER TO THE BOTTOM OF THE PAGE IF THE CONTENT IS NOT HIGH ENOUGH ***
 
+var docHeight = $(window).height();
+var footerHeight = $('#footer').height();
+var footerTop = $('#footer').position().top + footerHeight;
+
+if (footerTop < docHeight) {
+$('#footer').css('margin-top', (docHeight - footerTop) + 'px');
+}
 
 }
 
