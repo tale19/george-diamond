ab063590923251f8b23f4de3f39509f7d231b523
Stefan <stefan.tatic@yahoo.com>
2017-12-21 17:56:28 +0100

    index header: watch on YouTube button restyled to white, non-round

--- views/en/index.view.php
+++ views/en/index.view.php
@@ -26,7 +26,7 @@
     <img src="images/background.png" alt="Background image" class="img-responsive visible-xs mobile-background">
     <div id="superfinalist">
       <span>Serbia's Got Talent<span style="display:inline-block; font-family: sans-serif; font-size: 1em;">&trade;</span> superfinalist</span>
-      <a href="https://youtu.be/DSj25fuuE2o?t=6m8s" target="_blank" class="btn-lg btn-round">Watch on YouTube</a>
+      <a href="https://youtu.be/DSj25fuuE2o?t=6m8s" target="_blank" class="btn-lg">Watch on YouTube</a>
     </div> <!-- #superfinalist -->
   </div>  <!--container -->
 </div> <!--page-header -->
